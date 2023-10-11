e8f2c550d2fbc4cd7aed6b004713cf71464daa93
Zenahr <zenmatica@gmail.com>
2023-10-11 22:26:49 +0200

    initial draft.

---
+++ app/types.ts
@@ -0,0 +1,48 @@
+enum Throwable {
+    SMOKE = 'smoke',
+    FLASH = 'flash',
+    MOLLY = 'molly',
+    COMBO = 'combo'
+}
+
+enum Level {
+    MIRAGE = "mirage",
+    OVERPASS = "overpass",
+    VERTIGO = "vertigo",
+    ANCIENT = "ancient",
+    INFERNO = "inferno",
+    NUKE = "nuke",
+    ANUBIS = "anubis",
+    DUST2 = "dust2",
+    OFFICE = "office"
+}
+
+enum Site {
+    A = "a",
+    B = "b",
+    MID = "mid",
+    NONE = "none"
+}
+
+enum Team {
+    T = "t",
+    CT = "ct"
+}
+
+interface Video {
+    src: string;
+    location: string;
+    level: Level;
+    site: Site;
+    team: Team;
+    variation: number;
+    type: Throwable;
+}
+
+export {
+    Throwable,
+    Level,
+    Site,
+    Team,
+    Video
+}
